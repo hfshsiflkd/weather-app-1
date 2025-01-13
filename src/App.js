111ac665c2031aec76eed855623eca04a496a642
hfshsiflkd <toogiit0521@gmail.com>
2025-01-13 11:07:49 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Background from "./conformation/Background.js";
+import Background from "./components/Background.js";
 function App() {
   return (
     <div className="body">
-      <Background/>
-      <div class="flex h-screen">
-        <div class="flex-1 bg-customWhite flex items-center justify-center">
-          <p class="text-black">Цагаан хэсэг</p>
+      <Background />
+      <div className="flex h-screen">
+        <div className="flex-1 bg-customWhite flex items-center justify-center">
+          <p className="text-black">Цагаан хэсэг</p>
         </div>
 
-        <div class="flex-1 bg-customDark flex items-center justify-center">
-          <p class="text-white">Хар хэсэг</p>
+        <div className="flex-1 bg-customDark flex items-center justify-center">
+          <p className="text-white ">Хар хэсэг</p>
         </div>
       </div>
     </div>
