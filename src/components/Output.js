9fba610cea92caba305cd9eb3e2562f3fcf8c93b
hfshsiflkd <toogiit0521@gmail.com>
2025-01-20 11:13:30 +0800

    over
    over

--- src/components/Output.js
+++ src/components/Output.js
@@ -3,6 +3,7 @@ import { CitiesFilter } from "../utils/Citiesfilter";
 import { DayCard } from "./DayCard";
 import { NightCard } from "./NightCard";
 import { Search } from "./Search";
+import { Airpollution } from "./Airpollution";
 
 const WEATHER_API_KEY = "5b55bf7eca3c4e1b96622036251501";
 
@@ -14,7 +15,6 @@ const Output = (props) => {
   const [selectedCity, setSelectedCity] = useState("Ulaanbaatar");
   const [weatherLoding, setWeatherLoading] = useState(false);
   const [weather, setWeather] = useState({});
-
   const fetchData = async () => {
     setLoading(true);
     try {
@@ -35,10 +35,11 @@ const Output = (props) => {
 
     try {
       const response = await fetch(
-        ` https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${selectedCity}`,
+        ` https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${selectedCity}&aqi=yes&alerts=no`,
         { method: "get", headers: { "Content-type": "application/json" } }
       );
       const result = await response.json();
+      console.log(result);
 
       const weatherData = {
         max_c: result.forecast.forecastday[0].day.maxtemp_c,
@@ -47,6 +48,7 @@ const Output = (props) => {
         date: result.forecast.forecastday[0].date,
         icon: result.forecast.forecastday[0].day.condition.icon,
         city: result.location.name,
+        airquality: result.forecast.forecastday[0].day.air_quality.pm2_5,
       };
 
       setWeather(weatherData);
@@ -84,9 +86,15 @@ const Output = (props) => {
 
   return (
     <div className="flex h-screen">
-      <Search handlechange={handlechange} handleCityClick={handleCityClick} filteredData={filteredData}  countriesSeacrch={countriesSeacrch} />
+      <Search
+        handlechange={handlechange}
+        handleCityClick={handleCityClick}
+        filteredData={filteredData}
+        countriesSeacrch={countriesSeacrch}
+      />
       <DayCard weather={weather} weatherLoding={weatherLoding} />
       <NightCard weather={weather} weatherLoding={weatherLoding} />
+      <Airpollution selectedCity={selectedCity} weather={weather} weatherLoding={weatherLoding} />
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,36 +1,37 @@
-import Localization from "../icons/localization_icon"
+import Localization from "../icons/localization_icon";
 export const Search = (props) => {
-    const {handlechange,countriesSeacrch,handleCityClick,filteredData}= props
+  const { handlechange, countriesSeacrch, handleCityClick, filteredData } =
+    props;
   return (
     <div className="flex h-screen absolute w-screen justify-center overflow-hidden ">
-    <div className=" absolute z-40  flex flex-col  rounded-full justify-between items-center  ">
-      <div className=" z-40 w-[467px] h-[80px] p-[16px] pr-[60px] bg-white mr-[50px] flex  rounded-full justify-between items-center m-[100px] ml-[30px] mb-0">
-        <img
-          src="./img/Vector.png"
-          className="w-[34px] h-[34px] opacity-30"
-        />
-        <input
-          onChange={handlechange}
-          type="text"
-          value={countriesSeacrch}
-          placeholder="Search"
-          className="w-[350px] h-[40px] border-none text-3xl focus:outline-none "
-        />
-      </div>
-      <div className=" bg-white rounded-lg overflow-hidden mt-2.5 rounded-3xl bg-white/80 py-4 shadow-lg backdrop-blur-md mr-[20px] ">
-        {countriesSeacrch.length > 0 &&
-          filteredData.map((city, index) => (
-            <div
-              onClick={() => handleCityClick(city)}
-              key={index}
-              className="flex w-[400px] items-center gap-x-4 px-6 py-2 pl-0 transition-all duration-300 hover:bg-gray-100 cursor-pointer"
-            >
-              <Localization />
-              {city}
-            </div>
-          ))}
+      <div className=" absolute z-40  flex flex-col  rounded-full justify-between items-center  ">
+        <div className=" z-40 w-[467px] h-[80px] p-[16px] pr-[60px] bg-white mr-[50px] flex  rounded-full justify-between items-center m-[100px] ml-[30px] mb-0">
+          <img
+            src="./img/Vector.png"
+            className="w-[34px] h-[34px] opacity-30"
+          />
+          <input
+            onChange={handlechange}
+            type="text"
+            value={countriesSeacrch}
+            placeholder="Search"
+            className="w-[350px] h-[40px] border-none text-3xl focus:outline-none "
+          />
+        </div>
+        <div className=" bg-white rounded-lg overflow-hidden mt-2.5 rounded-3xl bg-white/80 py-4 shadow-lg backdrop-blur-md mr-[20px] ">
+          {countriesSeacrch.length > 0 &&
+            filteredData.map((city, index) => (
+              <div
+                onClick={() => handleCityClick(city)}
+                key={index}
+                className="flex w-[400px] items-center gap-x-4 px-6 py-2 pl-0 transition-all duration-300 hover:bg-gray-100 cursor-pointer"
+              >
+                <Localization />
+                {city}
+              </div>
+            ))}
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
